5cf04e5ee10a30c38dc68f19cda01ed69d724dfd
Michael Portuesi <mportuesisf@gmail.com>
2017-10-24 12:04:07 -0700

    fix bugs and make JS work according to page design.  store and query records now works, query supports exact match on artist name

--- indexeddb/js/main.js
+++ indexeddb/js/main.js
@@ -16,80 +16,85 @@ limitations under the License.
 
 'use strict';
 
-var indexedDB = window.indexedDB || window.webkitIndexedDB ||
+var iDB = window.indexedDB || window.webkitIndexedDB ||
     window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB;
 
 var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction ||
     window.OIDBTransaction || window.msIDBTransaction;
 
-var dbVersion = 1.0;
-
-var request = indexedDB.open('My songs', dbVersion);
+var dbVersion = 1;
 var db;
 
-function createObjectStore(database) {
-  console.log('Creating objectStore');
-  database.createObjectStore('songs');
-}
-
-function getImageFile() {
-  putElephantInDb('blah');
-}
-
-function getTransaction() {
-  var transaction;
-  try {
-    console.log('Try');
-  } catch (event0) {
-    try {
-      transaction = db.transaction(['songs'], IDBTransaction.READ_WRITE);
-      console.log('transaction: ', transaction);
-    } catch (event1) {
-      console.log('Error creating transaction: ', event1);
-    }
-  }
-  return transaction;
-}
-
-function putElephantInDb(blob) {
-  console.log('Putting elephants in IndexedDB');
-  var transaction = getTransaction();
-  //  var put = transaction.objectStore('songs').put(blob, 'image');
-  transaction.objectStore('songs').put(blob, 'image');
-  transaction.objectStore('songs').get('image').onsuccess = function(event) {
-    console.log(event.target.result);
-  };
-}
+var request = iDB.open('My songs', dbVersion);
 
 request.onerror = function(event) {
   console.log('Request error: ', event);
 };
 
 request.onsuccess = function(event) {
-  console.log('Request success: ', event);
+  console.log('Open request success: ', event);
   db = request.result;
   db.onerror = function(error) {
     console.log('Database error:', error);
   };
-  // Interim solution for Google Chrome to create an objectStore.
-  // Will be deprecated.
-  if (db.setVersion) {
-    if (db.version !== dbVersion) {
-      var setVersion = db.setVersion(dbVersion);
-      setVersion.onsuccess = function() {
-        createObjectStore(db);
-        getImageFile();
-      };
-    } else {
-      getImageFile();
-    }
-  } else {
-    getImageFile();
-  }
 };
 
-// For future use. Currently only in latest Firefox versions.
-// GI: might be good to have the versions for FF,Chrome and IE (10:) here.
 request.onupgradeneeded = function(event) {
   createObjectStore(event.target.result);
 };
+
+function createObjectStore(database) {
+  console.log('Creating objectStore');
+  var objectStore = database.createObjectStore('songs', { 'autoIncrement': true });
+	
+	console.log('Creating indices');
+  objectStore.createIndex('artist_idx', 'artist', { unique: false });
+	objectStore.createIndex('song_idx', 'song', { unique: false });
+}
+
+function getTransaction() {
+  var transaction;
+	try {
+	  transaction = db.transaction(['songs'], "readwrite");
+	  console.log('transaction: ', transaction);
+	} catch (event1) {
+	  console.log('Error creating transaction: ', event1);
+	}
+  return transaction;
+}
+
+function storeRecord(event) {
+	var artist = document.getElementById('artist').value;
+	var song = document.getElementById('song').value;
+	
+	console.log("Putting song in database");
+	var transaction = getTransaction();
+	var req = transaction.objectStore('songs').put( {'artist': artist, 'song': song } );
+	req.onsuccess = function(evt) {
+		console.log('Artist ' + artist + ' added, yay');
+	}
+}
+
+function findRecord(event) {
+	var query = document.getElementById('query').value;
+	
+	var transaction = getTransaction();
+	var objectStore = transaction.objectStore('songs');
+	var index = objectStore.index("artist_idx");
+	
+	index.openCursor(query).onsuccess = function(evt) {
+		var cursor = evt.target.result;
+		if (cursor) {
+			alert("Artist: " + cursor.value.artist + " Song: " + cursor.value.song );
+			cursor.continue();
+		} else {
+			alert("No more entries found.");
+		}
+	}
+}
+
+var storeButton = document.getElementById('storeButton');
+var findButton = document.getElementById('findButton');
+
+storeButton.onclick = storeRecord;
+findButton.onclick = findRecord;
